9e21de885036762fcb7c38455be1a11d70572cd8
Prince Ahmed <codingwithprince@gmail.com>
2021-09-03 21:51:46 +0600

    added header folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h3>Glowinroom</h3>
+      <Header></Header>
     </div>
   );
 }
